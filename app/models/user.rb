585f979b4b8c9b361f0a0568b67ee0eeeecad96f
bellom <bellomsean@gmail.com>
2019-05-27 21:33:38 +0100

    sessions and signin half done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  before_create :remember
   has_secure_password
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(:remember_digest).is_password?(remember_token)
+  end
+
+  def self.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+private 
+
+  def remember
+    self.remember_token = User.new_token
+    self.remember_token = User.encrypt(remember_token)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_145811) do
+ActiveRecord::Schema.define(version: 2019_05_27_162656) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
