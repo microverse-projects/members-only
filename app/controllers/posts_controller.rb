366a92f43357d450ab62e6ca9463c86679a3998c
bellom <bellomsean@gmail.com>
2019-05-28 14:45:20 +0100

    AUTH and Post half done

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+
+  before_action :logged_in_user, only: [:new, :create]
+
+  def new
+    @post = Post.new
+  end
+
+  def create 
+    @post = current_user.posts.build(post_params)
+      if @post.save
+        redirect_to posts_path
+      else 
+        render 'new'
+      end
+  end
+
+
+  def logged_in_user
+    unless logged_in? 
+      redirect_to login_url
+    end
+  end
+
+end
+
+private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
